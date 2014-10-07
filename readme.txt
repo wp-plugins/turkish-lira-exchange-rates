720dbbceed4143d655eacdd68f025c1f3434f2d9
incirusa <incirusa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-07 07:07:38 +0000

    Updated the plugin based on the new format of the currency XML.
    git-svn-id: https://plugins.svn.wordpress.org/turkish-lira-exchange-rates/trunk@1003095 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: incirusa
 Donate link: http://www.iwasinturkey.com/buy-us-a-beer
 Tags: turkey,turkish,lira,currency,exchange rates,YTL,TL,TRY, doviz kuru,yeni turk lirasi,doviz,yeniturk lirasi,widget,plugin,sidebar
 Requires at least: 2.8
-Tested up to: 3.8
-Stable tag: 2.2
+Tested up to: 4
+Stable tag: 3
 
 
 
--- turkishlira.php
+++ turkishlira.php
@@ -4,7 +4,7 @@ Plugin Name: Turkish Lira Exchange Rates
 Plugin URI: http://lycie.com
 Description: Display daily exchange rates from the Central Bank of Turkey (Türkiye Cumhuriyet Merkez Bankasi).
 Author: Onur Kocatas
-Version: 2.2
+Version: 3
 Author URI: http://lycie.com
 */
 
@@ -36,7 +36,6 @@ class Turkish_Lira_Widget extends WP_Widget {
 		$title = apply_filters('widget_title', $instance['title'] );
 		$buyLabel = $instance['buyLabel'];
 		$sellLabel = $instance['sellLabel'];
-		$updateLabel = $instance['updateLabel'];
 		$add_CSS = isset( $instance['add_CSS'] ) ? $instance['add_CSS'] : true;
 		$display_AUD = isset( $instance['display_AUD'] ) ? $instance['display_AUD'] : true;
 		$display_CAD = isset( $instance['display_CAD'] ) ? $instance['display_CAD'] : true;
@@ -64,128 +63,106 @@ class Turkish_Lira_Widget extends WP_Widget {
 		/*Css*/
 		if ( $add_CSS )
 			echo '
-		<style type="text/css">
-#currency {margin:0 auto;}
+<style type="text/css">
+#currency {margin:0 auto;background: #fff; color: #000;padding: 0 3px;}
 #currency div.c_header {clear:both;height:17px;}
-#currency div.c_row{font-size:1em;height:17px;padding:0;font-weight:normal;clear:both;}
+#currency div.c_row{font-size:1em;height:17px;padding:0;font-weight:normal;clear:both;overflow: hidden;}
 #currency div.odd {background:#efefef;}
-#currency div.c_row .c_symbol{float:left;}
+#currency div.c_row .c_symbol{float:left;padding-left:20px;background:transparent no-repeat scroll 0 3px;}
 #currency div.difi{background:#efefef;}
-#currency div.c_row div.c_rate{min-width:50px;float:right;text-align:right;padding:0px 4px;}
+#currency div.c_row div.c_rate{min-width:50px;float:right;text-align:right;padding:0px 4px 0 0;}
 #currency div.c_header div.c_rate{min-width:50px;float:right;text-align:center;font-weight:bold;padding:0px;}
-#currency div.c_row .AUD{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/AUD.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .CAD{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/CAD.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .CHF{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/CHF.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .DKK{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/DKK.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .EUR{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/EUR.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .GBP{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/GBP.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .JPY{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/JPY.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .KWD{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/KWD.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .NOK{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/NOK.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .SAR{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/SAR.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .SEK{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/SEK.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_row .USD{background:transparent url(wp-content/plugins/turkish-lira-exchange-rates/USD.gif) no-repeat scroll 0 3px;padding-left:20px;}
-#currency div.c_info{font-size:0.9em;color:#999;clear:both;text-align:center;font-weight:bold;}
+#currency div.c_row .AUD{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/AUD.gif);}
+#currency div.c_row .CAD{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/CAD.gif);}
+#currency div.c_row .CHF{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/CHF.gif);}
+#currency div.c_row .DKK{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/DKK.gif);}
+#currency div.c_row .EUR{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/EUR.gif);}
+#currency div.c_row .GBP{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/GBP.gif);}
+#currency div.c_row .JPY{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/JPY.gif);}
+#currency div.c_row .KWD{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/KWD.gif);}
+#currency div.c_row .NOK{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/NOK.gif);}
+#currency div.c_row .SAR{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/SAR.gif);}
+#currency div.c_row .SEK{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/SEK.gif);}
+#currency div.c_row .USD{background-image:url(wp-content/plugins/turkish-lira-exchange-rates/USD.gif);}
 #currency div.c_credit{font-size:0.9em;color:#999;clear:both;text-align:center;font-weight:bold;display:none;}
 #currency div.c_credit a{color:#369;clear:both;text-align:center;font-weight:bold;}
-		</style>';
-		$rates=fopen("http://www.tcmb.gov.tr/kurlar/today.html","r") or die ("Something is wrong");
-		$AUD="AUD/TRY";
-		$CAD="CAD/TRY";
-		$CHF="CHF/TRY";
-		$DKK="DKK/TRY";
-		$EUR="EUR/TRY";
-		$GBP="GBP/TRY";
-		$JPY="JPY/TRY";
-		$KWD="KWD/TRY";
-		$NOK="NOK/TRY";
-		$SAR="SAR/TRY";
-		$SEK="SEK/TRY";
-		$USD="USD/TRY";
-		$DATE="Indicative Exchange Rates Announced at ";
-		while (!feof($rates)) {
-			$row= fgets($rates, 128);
+</style>';
+		
+$rates=fopen("http://www.tcmb.gov.tr/kurlar/today.xml","r") or die ("Something is wrong");
+$AUD="AUSTRALIAN DOLLAR";
+$CAD="CANADIAN DOLLAR";
+$CHF="SWISS FRANK";
+$DKK="DANISH KRONE";
+$EUR="EURO";
+$GBP="POUND STERLING";
+$JPY="JAPENESE YEN";
+$KWD="KUWAITI DINAR";
+$NOK="NORWEGIAN KRONE";
+$SAR="SAUDI RIYAL";
+$SEK="SWEDISH KRONA";
+$USD="US DOLLAR";
 
-			if ($findAUD=strpos($row,$AUD)===false){
-			}else{
-				$findAUD=strpos($row,$AUD);
-				$AUDbuy=number_format(trim(substr($row,$findAUD+46,6)),4);
-				$AUDsell=number_format(trim(substr($row,$findAUD+61,6)),4);
-			}
-			if ($findCAD=strpos($row,$CAD)===false){
-			}else{
-				$findCAD=strpos($row,$CAD);
-				$CADbuy=number_format(trim(substr($row,$findCAD+46,6)),4);
-				$CADsell=number_format(trim(substr($row,$findCAD+61,6)),4);
-			}
-			if ($findCHF=strpos($row,$CHF)===false){
-			}else{
-				$findCHF=strpos($row,$CHF);
-				$CHFbuy=number_format(trim(substr($row,$findCHF+46,6)),4);
-				$CHFsell=number_format(trim(substr($row,$findCHF+61,6)),4);
-			}
-			if ($findDKK=strpos($row,$DKK)===false){
-			}else{
-				$findDKK=strpos($row,$DKK);
-				$DKKbuy=number_format(trim(substr($row,$findDKK+46,6)),4);
-				$DKKsell=number_format(trim(substr($row,$findDKK+61,6)),4);
-			}
-			if ($findEUR=strpos($row,$EUR)===false){
-			}else{
-				$findEUR=strpos($row,$EUR);
-				$EURbuy=number_format(trim(substr($row,$findEUR+46,6)),4);
-				$EURsell=number_format(trim(substr($row,$findEUR+61,6)),4);
-			}
-			if ($findGBP=strpos($row,$GBP)===false){
-			}else{
-				$findGBP=strpos($row,$GBP);
-				$GBPbuy=number_format(trim(substr($row,$findGBP+46,6)),4);
-				$GBPsell=number_format(trim(substr($row,$findGBP+61,6)),4);
-			}
-			if ($findJPY=strpos($row,$JPY)===false){
-			}else{
-				$findJPY=strpos($row,$JPY);
-				$JPYbuy=number_format(trim(substr($row,$findJPY+46,6)),4);
-				$JPYsell=number_format(trim(substr($row,$findJPY+61,6)),4);
-			}
-			if ($findKWD=strpos($row,$KWD)===false){
-			}else{
-				$findKWD=strpos($row,$KWD);
-				$KWDbuy=number_format(trim(substr($row,$findKWD+46,6)),4);
-				$KWDsell=number_format(trim(substr($row,$findKWD+61,6)),4);
-			}
-			if ($findNOK=strpos($row,$NOK)===false){
-			}else{
-				$findNOK=strpos($row,$NOK);
-				$NOKbuy=number_format(trim(substr($row,$findNOK+46,6)),4);
-				$NOKsell=number_format(trim(substr($row,$findNOK+61,6)),4);
-			}
-			if ($findSAR=strpos($row,$SAR)===false){
-			}else{
-				$findSAR=strpos($row,$SAR);
-				$SARbuy=number_format(trim(substr($row,$findSAR+46,6)),4);
-				$SARsell=number_format(trim(substr($row,$findSAR+61,6)),4);
-			}
-			if ($findSEK=strpos($row,$SEK)===false){
-			}else{
-				$findSEK=strpos($row,$SEK);
-				$SEKbuy=number_format(trim(substr($row,$findSEK+46,6)),4);
-				$SEKsell=number_format(trim(substr($row,$findSEK+61,6)),4);
-			}
-			if ($findUSD=strpos($row,$USD)===false){
-			}else{
-				$findUSD=strpos($row,$USD);
-				$USDbuy=number_format(trim(substr($row,$findUSD+46,6)),4);
-				$USDsell=number_format(trim(substr($row,$findUSD+61,6)),4);
-			}
-			if ($findDATE=strpos($row,$DATE)===false){
-			}else{
-				$findDATE=strpos($row,$DATE);
-				$ondegis = array(' on ' => ' - ');
-				$DATEdisplay=strtr(trim(substr($row,$findDATE+39,+19)),$ondegis);
-			}
+if ($rates == false) {
 
+} else {
+
+
+	$xml = simplexml_load_file('http://www.tcmb.gov.tr/kurlar/today.xml');
+
+	foreach($xml->Currency as $Currency) {
+		$currency = $Currency->{'CurrencyName'};	
+		switch ($currency) {
+			case $AUD:
+			$AUDbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$AUDsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $CAD:
+			$CADbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$CADsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $CHF:
+			$CHFbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$CHFsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $EUR:
+			$EURbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$EURsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $DKK:
+			$DKKbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$DKKsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $GBP:
+			$GBPbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$GBPsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $JPY:
+			$JPYbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$JPYsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $KWD:
+			$KWDbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$KWDsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $NOK:
+			$NOKbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$NOKsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $SAR:
+			$SARbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$SARsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $SEK:
+			$SEKbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$SEKsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
+			case $USD:
+			$USDbuy = number_format(substr(trim($Currency->{'ForexBuying'}),0,4),4);
+			$USDsell = number_format(substr(trim($Currency->{'ForexSelling'}),0,4),4);
+			break;
 		}
+	}
+}
 		fclose($rates);
 		
 		echo '<div id="currency"><div class="c_header"><div class="c_rate">';
@@ -198,47 +175,17 @@ class Turkish_Lira_Widget extends WP_Widget {
 		
 
 		echo '<div class="c_row difi"><div class="c_symbol AUD">AUD</div><div class="c_rate">'.$AUDsell.'</div><div class="c_rate">'.$AUDbuy.'</div></div>';
-
-
 		echo '<div class="c_row"><div class="c_symbol CAD">CAD</div><div class="c_rate">'.$CADsell.'</div><div class="c_rate">'.$CADbuy.'</div></div>';
-
-
 		echo '<div class="c_row difi"><div class="c_symbol CHF">CHF</div><div class="c_rate">'.$CHFsell.'</div><div class="c_rate">'.$CHFbuy.'</div></div>';
-
-
 		echo '<div class="c_row "><div class="c_symbol DKK">DKK</div><div class="c_rate">'.$DKKsell.'</div><div class="c_rate">'.$DKKbuy.'</div></div>';
-
-
 		echo '<div class="c_row difi"><div class="c_symbol EUR">EUR</div><div class="c_rate">'.$EURsell.'</div><div class="c_rate">'.$EURbuy.'</div></div>';
-
-
 		echo '<div class="c_row"><div class="c_symbol GBP">GBP</div><div class="c_rate">'.$GBPsell.'</div><div class="c_rate">'.$GBPbuy.'</div></div>';
-
-
 		echo '<div class="c_row difi"><div class="c_symbol JPY">JPY</div><div class="c_rate">'.$JPYsell.'</div><div class="c_rate">'.$JPYbuy.'</div></div>';
-
-
 		echo '<div class="c_row"><div class="c_symbol KWD">KWD</div><div class="c_rate">'.$KWDsell.'</div><div class="c_rate">'.$KWDbuy.'</div></div>';
-
-
 		echo '<div class="c_row difi"><div class="c_symbol NOK">NOK</div><div class="c_rate">'.$NOKsell.'</div><div class="c_rate">'.$NOKbuy.'</div></div>';
-
-
 		echo '<div class="c_row"><div class="c_symbol SAR">SAR</div><div class="c_rate">'.$SARsell.'</div><div class="c_rate">'.$SARbuy.'</div></div>';
-
-
 		echo '<div class="c_row difi"><div class="c_symbol SEK">SEK</div><div class="c_rate">'.$SEKsell.'</div><div class="c_rate">'.$SEKbuy.'</div></div>';
-
-
 		echo '<div class="c_row"><div class="c_symbol USD">USD</div><div class="c_rate">'.$USDsell.'</div><div class="c_rate">'.$USDbuy.'</div></div>';
-
-
-
-		echo '<div class="c_info">';
-		if ( $updateLabel )
-			echo '' . $updateLabel . '';
-		echo ''.$DATEdisplay.'</div>';
-
 		echo '<div class="c_credit"><a href="http://www.iwasinturkey.com/turkish-lira-exchange-rates">Turkish Lira Exchange Rates</a></div></div>';
 
 		/* After widget (defined by themes). */
@@ -253,7 +200,6 @@ class Turkish_Lira_Widget extends WP_Widget {
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['buyLabel'] = $new_instance['buyLabel'];
 		$instance['sellLabel'] = $new_instance['sellLabel'];
-		$instance['updateLabel'] = $new_instance['updateLabel'];
 		$instance['add_CSS'] = $new_instance['add_CSS'];
 		$instance['display_AUD'] = $new_instance['display_AUD'];
 		$instance['display_CAD'] = $new_instance['display_CAD'];
@@ -276,7 +222,7 @@ class Turkish_Lira_Widget extends WP_Widget {
 	function form( $instance ) {
 
 		/* Set up some default widget settings. */
-		$defaults = array( 'title' => 'Turkish Lira Exchange Rates', 'buyLabel' => 'BUY', 'sellLabel' => 'SELL', 'updateLabel' => 'Updated on ',  'add_CSS' => true,'display_AUD' => false ,'display_CAD' => true ,'display_CHF' => true ,'display_DKK' => true ,'display_EUR' => true ,'display_GBP' => true ,'display_JPY' => true ,'display_KWD' => true ,'display_NOK' => true ,'display_SAR' => true ,'display_SEK' => true ,'display_USD' => true ,'display_date' => true ,'display_credit' => true );
+		$defaults = array( 'title' => 'Turkish Lira Exchange Rates', 'buyLabel' => 'BUY', 'sellLabel' => 'SELL', 'add_CSS' => true,'display_AUD' => false ,'display_CAD' => true ,'display_CHF' => true ,'display_DKK' => true ,'display_EUR' => true ,'display_GBP' => true ,'display_JPY' => true ,'display_KWD' => true ,'display_NOK' => true ,'display_SAR' => true ,'display_SEK' => true ,'display_USD' => true ,'display_date' => true ,'display_credit' => true );
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
 		<p>
@@ -290,10 +236,6 @@ class Turkish_Lira_Widget extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id( 'sellLabel' ); ?>">Label for SELL:</label>
 			<input id="<?php echo $this->get_field_id( 'sellLabel' ); ?>" name="<?php echo $this->get_field_name( 'sellLabel' ); ?>" value="<?php echo $instance['sellLabel']; ?>" style="width:100%;" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'updateLabel' ); ?>">Label for Update on:</label>
-			<input id="<?php echo $this->get_field_id( 'updateLabel' ); ?>" name="<?php echo $this->get_field_name( 'updateLabel' ); ?>" value="<?php echo $instance['updateLabel']; ?>" style="width:100%;" />
 		</p>
 		<?php
 	}
